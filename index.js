8a73a3fd80913a50c2884bdabafd5096a6b94a86
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-07-05 17:27:29 +0200

    leaderbord display

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const computerResult = document.getElementById("computer-choice")
 const result = document.getElementById("result")
 const playerScore = document.getElementById("player-score")
 const iaScore = document.getElementById("ia-score")
+// leaderbord
+const round = document.getElementById("round")
+const playerRound = document.getElementById("player-round")
+const iaRound = document.getElementById("ia-round")
 
 let playerChoice
 let iaChoice
 let resultat 
 let playerPoints = 0
 let iaPoints = 0
-
+// leaderbord
+let roundNumber = 0
+let playerRoundWin = 0
+let iaRoundWin = 0
 
 elementChoice.forEach(elementChoice => elementChoice.addEventListener('click',(e) => {
     // récupération de l'ID du boutton cliqué
@@ -52,29 +59,38 @@ const verification = () => {
             (playerChoice === "ciseaux" && iaChoice === "pierre")){
                 resultat = "l'ordinateur a gagné"
                 iaPoints += 1
+                roundNumber += 1
     }else{
         resultat = "Vous avez gagné !"
         playerPoints += 1
+        roundNumber += 1
     }
     
     iaScore.innerHTML = iaPoints
     playerScore.innerHTML = playerPoints
+    round.innerHTML = roundNumber
+    iaRound.innerHTML = iaChoice
+    playerRound.innerHTML = playerChoice
+    
+    
 
-    if(playerPoints === 5){
-        playerPoints = 0
-        iaPoints = 0
+    if(playerPoints === 3){
         iaScore.innerHTML = iaPoints
         playerScore.innerHTML = playerPoints
-    }
-
-    if(iaPoints === 5){
+        round.innerHTML = roundNumber
         playerPoints = 0
         iaPoints = 0
-        iaScore.innerHTML = iaPoints
-        playerScore.innerHTML = playerPoints
+        roundNumber = 0
     }
 
+    if(iaPoints === 3){
+        iaScore.innerHTML = iaPoints
+        playerScore.innerHTML = playerPoints
+        round.innerHTML = roundNumber
+        playerPoints = 0
+        iaPoints = 0
+        roundNumber = 0
+    }
 
     result.innerHTML = resultat
-    
 }
